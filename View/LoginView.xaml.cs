3a2843813de2b2a932f2c1a931c93970dfa1b874
Gabriel <grpc15@outlook.com>
2025-07-05 15:29:29 -0300

    enhancement: organiza LoginViewModel

--- View/LoginView.xaml.cs
+++ View/LoginView.xaml.cs
@@ -1,60 +1,32 @@
-﻿using aluguel_de_imoveis_wpf.Communication.Response;
-using aluguel_de_imoveis_wpf.Security;
-using aluguel_de_imoveis_wpf.Services;
-using System.Net.Http;
-using System.Text.Json;
+﻿using aluguel_de_imoveis_wpf.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace aluguel_de_imoveis_wpf.View
 {
     public partial class LoginView : UserControl
     {
-        private readonly UsuarioService _usuarioService;
-        private MainWindow _mainWindow;
-
         public LoginView(MainWindow mainWindow)
         {
             InitializeComponent();
-            _usuarioService = new UsuarioService();
-            _mainWindow = mainWindow;
+            DataContext = new LoginViewModel(
+                abrirPainel: mainWindow.AbrirPainel,
+                abrirRegistrar: mainWindow.AbrirRegistrar
+            );
         }
 
-        private void PasswordBox_MouseDown(object sender, MouseButtonEventArgs e)
+        private void PasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
         {
-            PasswordBox.Focus();
-        }
+            var passwordBox = sender as PasswordBox;
 
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-        {
-            PlaceholderText.Visibility = string.IsNullOrEmpty(PasswordBox.Password) ? Visibility.Visible : Visibility.Collapsed;
-        }
+            PlaceholderText.Visibility = string.IsNullOrEmpty(passwordBox!.Password)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
 
-        private void EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            EmailPlaceholderText.Visibility = string.IsNullOrEmpty(EmailTextBox.Text) ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        private async void LoginButtonClick(object sender, RoutedEventArgs e)
-        {
-            try
+            if (this.DataContext is LoginViewModel viewModel)
             {
-                var response = await _usuarioService.LoginAsync(EmailTextBox.Text, PasswordBox.Password);
-
-                TokenStorage.SaveToken(response.Token);
-
-                _mainWindow.AbrirPainel();
+                viewModel.Senha = passwordBox.Password;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void OnRegistrarClick(object sender, RoutedEventArgs e)
-        {
-            _mainWindow.AbrirRegistrar();
         }
     }
 }
---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,73 @@
+﻿using aluguel_de_imoveis_wpf.Security;
+using aluguel_de_imoveis_wpf.Services;
+using aluguel_de_imoveis_wpf.Utils;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace aluguel_de_imoveis_wpf.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _email = string.Empty;
+        private string _senha = string.Empty;
+        private readonly UsuarioService _usuarioService;
+        private readonly Action _abrirPainel;
+        private readonly Action _abrirRegistrar;
+
+        public LoginViewModel(Action abrirPainel, Action abrirRegistrar)
+        {
+            _usuarioService = new UsuarioService();
+            _abrirPainel = abrirPainel;
+            _abrirRegistrar = abrirRegistrar;
+
+            LoginCommand = new RelayCommand(async (_) => await LoginAsync());
+            RegistrarCommand = new RelayCommand(_ => _abrirRegistrar());
+        }
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Senha
+        {
+            get => _senha;
+            set
+            {
+                _senha = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+        public ICommand RegistrarCommand { get; }
+
+        private async Task LoginAsync()
+        {
+            try
+            {
+                var response = await _usuarioService.LoginAsync(Email, Senha);
+                TokenStorage.SaveToken(response.Token);
+                _abrirPainel();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void OnPropertyChanged([CallerMemberName] string nome = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nome));
+        }
+    }
+}
